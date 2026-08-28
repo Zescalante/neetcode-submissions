fa1afd857fd1eb8c3075c6ff0e9b434010491765
Zacharias Escalante <52757009+Zescalante@users.noreply.github.com>
2026-08-28 10:12:13 -0400

    Add: sql-customer-placing-the-largest-number-of-orders - submission-1

---
+++ SQL/sql-customer-placing-the-largest-number-of-orders/submission-1.sql
@@ -0,0 +1,18 @@
+-- SELECT
+--     customer_number
+-- FROM (
+-- SELECT
+--     customer_number,
+--     COUNT(*) AS order_cnt
+-- FROM orders
+-- GROUP BY customer_number
+-- ) AS S
+-- ORDER BY order_cnt DESC
+-- LIMIT 1
+
+SELECT
+    customer_number
+FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
