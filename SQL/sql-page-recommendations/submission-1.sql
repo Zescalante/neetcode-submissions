c1c262b7aa5b77559d5e604842c9b9502215b793
Zacharias Escalante <52757009+Zescalante@users.noreply.github.com>
2026-08-30 14:53:44 -0400

    Add: sql-page-recommendations - submission-1

---
+++ SQL/sql-page-recommendations/submission-1.sql
@@ -0,0 +1,17 @@
+WITH one_friends AS (
+SELECT
+    user1_id AS id
+FROM friendship
+WHERE user2_id = 1
+UNION
+SELECT
+    user2_id AS id
+FROM friendship
+WHERE user1_id = 1
+)
+
+SELECT DISTINCT
+    L.page_id AS recommended_page
+FROM one_friends F
+JOIN likes L ON F.id = L.user_id
+WHERE NOT EXISTS (SELECT 1 FROM likes L2 WHERE L2.user_id = 1 AND L.page_id = L2.page_id)
