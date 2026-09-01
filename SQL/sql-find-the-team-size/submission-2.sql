525452fb9729cc6859900998fef4560278794a98
Zacharias Escalante <52757009+Zescalante@users.noreply.github.com>
2026-09-01 10:23:14 -0400

    Add: sql-find-the-team-size - submission-2

---
+++ SQL/sql-find-the-team-size/submission-2.sql
@@ -0,0 +1,18 @@
+-- WITH team_sizes AS (
+-- SELECT  
+--     team_id,
+--     COUNT(*) AS team_size
+-- FROM employee
+-- GROUP BY team_id
+-- )
+
+-- SELECT
+--     E.employee_id,
+--     T.team_size
+-- FROM employee E
+-- JOIN team_sizes T ON E.team_id = T.team_id
+
+SELECT
+    employee_id,
+    COUNT(*) OVER (PARTITION BY team_id) AS team_size
+FROM employee
